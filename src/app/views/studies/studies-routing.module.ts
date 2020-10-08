46be995321b63713c959e4dbe093fc468242d657
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2020-10-07 23:55:02 -0300

    new and confirm study

---
+++ src/app/views/studies/studies-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NewStudyComponent } from './newStudy/newStudy.component';
+import { ConfirmStudyComponent } from './confirmStudy/confirmStudy.component';
+
+
+const routes: Routes = [
+  {
+    path: 'newstudy',
+    component: NewStudyComponent,
+    data: {
+      title: 'newStudy'
+    }
+  },
+    {
+    path: 'confirmstudy',
+    component: ConfirmStudyComponent,
+    data: {
+      title: 'confirmStudy'
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class StudiesRoutingModule {}
---
+++ src/app/views/studies/studies.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
+import { NewStudyComponent } from './newStudy/newStudy.component';
+import { ConfirmStudyComponent } from './confirmStudy/confirmStudy.component';
+import { StudiesRoutingModule } from './studies-routing.module';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonModule} from '@angular/common';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgSelectModule,
+    StudiesRoutingModule,
+    ChartsModule,
+    BsDropdownModule,
+    ButtonsModule.forRoot()
+  ],
+  declarations: [
+      NewStudyComponent,
+      ConfirmStudyComponent]
+})
+export class StudiesModule { }
