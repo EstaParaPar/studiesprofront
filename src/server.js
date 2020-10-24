07dd47d6111f08bf3ceddbfc979b841d8d2ebcc3
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2020-10-24 20:18:07 -0300

    prod

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/sp'));
+app.use(express.static(__dirname+'/dist/studiesprofront'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/sp/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/studiesprofront/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
