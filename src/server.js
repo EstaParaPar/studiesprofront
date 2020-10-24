a562603fb7a41d7f820f8a947395aba5a77cf50a
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2020-10-24 20:20:24 -0300

    prod

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
+const express = require('express');
+
 const app = express();
 
-app.use(express.static(__dirname+'/dist/studiesprofront'));
-app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/studiesprofront/index.html'));
-});
+app.use(express.static('./dist/studiesprofront'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/studiesprofront/'}),
+);
 
 app.listen(process.env.PORT || 8080);
